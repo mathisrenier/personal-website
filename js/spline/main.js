8d134709405a8143d6ce26ebf206a886dc6bb204
Mathis Renier <reniermathis@gmail.com>
2021-05-26 21:26:55 +0200

    3d compatibility for mobile

--- js/spline/main.js
+++ js/spline/main.js
@@ -1,4 +1,12 @@
 
+			const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+
 			const app = new SpeRuntime.Application();
-			app.start('./scene.json');
+
+			if(vw < 600) {
+				app.start('./js/scene-phone.json');
+			}
+			else {
+				app.start('./js/scene-desktop.json');
+			}
         
