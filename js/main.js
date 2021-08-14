e467f71e5ff8a025ba1c7bc84ac950edc00a5c4f
Mathis Renier <reniermathis@gmail.com>
2021-08-14 17:44:09 +0200

    added smooth scrool for safary

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+function aboutClick() {
+  document.querySelector('#about-me').scrollIntoView({ behavior: 'smooth' });
+}
+
+
+function projClick() {
+  document.querySelector('#projects').scrollIntoView({ behavior: 'smooth' });
+}
+
+function contactClick() {
+  document.querySelector('#contact').scrollIntoView({ behavior: 'smooth' });
+}
